3cc274955d9575186a733a7197a3c072867f8654
seulgi9834 <seulgi9834@naver.com>
2022-03-19 23:45:34 +0900

    UPDATE : BOJ_1261 알고스팟
    12388KB 108ms 2143B
    https://www.acmicpc.net/problem/1261

---
+++ Java/BOJ/1261_알고스팟/1261_알고스팟.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M, Min;
+	static int[][] map;
+	static int[][] move = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static boolean[][] visited;
+	static ArrayList<position> wall;
+
+	static class position implements Comparable<position> {
+		int row;
+		int col;
+		int crush;
+
+		public position(int row, int col, int crush) {
+			super();
+			this.row = row;
+			this.col = col;
+			this.crush = crush;
+		}
+
+		@Override
+		public int compareTo(position o) {
+			if (o.crush < crush) {
+				return 1;
+			} else if (o.crush > crush) {
+				return -1;
+			}
+			return 0;
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		Min = Integer.MAX_VALUE;
+		map = new int[N + 1][M + 1];
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i + 1][j + 1] = str.charAt(j) - '0';
+			}
+		}
+
+		visited = new boolean[N + 1][M + 1];
+
+		System.out.println(dfs(1, 1));
+
+	}
+
+	static int dfs(int r, int c) {
+		PriorityQueue<position> queue = new PriorityQueue<>();
+		queue.add(new position(r, c, 0));
+		visited[r][c] = true;
+		while (!queue.isEmpty()) {
+			position pos = queue.poll();
+			if (pos.row == N && pos.col == M) {
+				return Math.min(Min, pos.crush);
+			}
+			for (int i = 0; i < 4; i++) {
+				int newR = pos.row + move[i][0];
+				int newC = pos.col + move[i][1];
+				if (newR > N || newC > M || newR < 1 || newC < 1) {
+					continue;
+				}
+				if (map[newR][newC] == 0) {
+					if (!visited[newR][newC]) {
+						visited[newR][newC] = true;
+						queue.add(new position(newR, newC, pos.crush));
+					}
+				} else if (map[newR][newC] == 1) {
+					if (!visited[newR][newC]) {
+						visited[newR][newC] = true;
+						queue.add(new position(newR, newC, pos.crush + 1));
+					}
+				}
+			}
+		}
+		return 0;
+	}
+
+}
