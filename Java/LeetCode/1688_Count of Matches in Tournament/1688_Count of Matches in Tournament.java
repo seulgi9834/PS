11d83b38048bdff8fa5ea72c3c6916af0de0fead
seulgi9834 <seulgi9834@naver.com>
2022-03-27 17:10:22 +0900

    UPDATE : LEETCODE_1688 Count of Matches in Tournament
    39MB 0ms
    https://leetcode.com/problems/count-of-matches-in-tournament/

---
+++ Java/LeetCode/1688_Count of Matches in Tournament/1688_Count of Matches in Tournament.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int sum=0;
+        while(n!=1){
+            if(n%2==1){
+                sum+=n/2;
+                n=n/2+1;
+            }
+            else{
+                sum+=n/2;
+                n=n/2;
+            }
+        }
+        return sum;
+    }
+}
