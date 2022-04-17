f087eee515f4a82b29e5594d9074e1e35c660a15
seulgi9834 <seulgi9834@naver.com>
2022-04-17 22:15:26 +0900

    UPDATE : BOJ_11403 경로 찾기
    21904KB 328ms 866B
    https://www.acmicpc.net/problem/11403

---
+++ Java/BOJ/11403_경로찾기/11403_경로찾기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int r = 0; r < N; r++) {// 경
+			for (int j = 0; j < N; j++) {// 도
+				for (int i = 0; i < N; i++) {// 출
+					if (map[i][r] == 1 && map[r][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
