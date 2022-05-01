005375400b953e0b970edb311652126b804fbca5
seulgi9834 <seulgi9834@naver.com>
2022-05-01 20:13:04 +0900

    UPDATE : BOJ_25083 새싹
    11244KB 72ms 328B
    https://www.acmicpc.net/problem/25083

---
+++ Java/BOJ/25083_새싹/25083_새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |");
+		System.out.println(sb);
+	}
+}
